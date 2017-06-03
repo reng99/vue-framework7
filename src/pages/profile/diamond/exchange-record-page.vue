647071f71396b4a0a33cbf5bfac63dbc7bcebda4
reng <1837895991@qq.com>
2017-06-03 23:58:23 +0800

    add new

--- src/pages/profile/diamond/exchange-record-page.vue
+++ src/pages/profile/diamond/exchange-record-page.vue
@@ -1,18 +1,32 @@
 
 <template>
-  <f7-page>
-    <f7-navbar title="兑换记录" back-link="返回" sliding>
-    </f7-navbar>
+  <f7-page id="exchange-record-page">
+    <f7-navbar title="兑换记录" back-link="返回" sliding></f7-navbar>
+    <f7-list>
+        <f7-list-item v-for="(item,key) in imitateList" :title="item.name" :key="key">
+            <div class="item-after">Label</div>
+        </f7-list-item>
+    </f7-list>
   </f7-page>
 </template>
 
 <script>
 export default {
     data(){
-        return {}
+        return {
+            imitateList:[
+                {name:"积分兑换",time:"2017-06-03",amount:"100"},
+                {name:"积分兑换",time:"2017-06-03",amount:"100"},
+                {name:"积分兑换",time:"2017-06-03",amount:"100"}
+            ]
+        }
     },
 }
 </script>
 <style lang="less" scoped>
-    
+    #exchange-record-page{
+        .list-block{
+            margin:0;
+        }
+    }
 </style>
