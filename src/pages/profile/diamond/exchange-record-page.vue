9c75501e5a4e7f0513420078120159800284bb89
reng <1837895991@qq.com>
2017-06-03 23:59:36 +0800

    add the list-record

--- src/pages/profile/diamond/exchange-record-page.vue
+++ src/pages/profile/diamond/exchange-record-page.vue
@@ -4,7 +4,7 @@
     <f7-navbar title="兑换记录" back-link="返回" sliding></f7-navbar>
     <f7-list>
         <f7-list-item v-for="(item,key) in imitateList" :title="item.name" :key="key">
-            <div class="item-after">Label</div>
+            <div class="item-after">+{{item}}</div>
         </f7-list-item>
     </f7-list>
   </f7-page>
@@ -14,7 +14,7 @@
 export default {
     data(){
         return {
-            imitateList:[
+            imitateList:[// 数据添加
                 {name:"积分兑换",time:"2017-06-03",amount:"100"},
                 {name:"积分兑换",time:"2017-06-03",amount:"100"},
                 {name:"积分兑换",time:"2017-06-03",amount:"100"}
