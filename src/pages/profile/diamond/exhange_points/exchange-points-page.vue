28aa6285b6855e6fcf5230b9fb418ffd5cf88751
reng <1837895991@qq.com>
2017-06-10 08:56:28 +0800

    init exchange points page into

---
+++ src/pages/profile/diamond/exhange_points/exchange-points-page.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="exhange-points-page"></div>
+</template>
+<script>
+    export default{
+        name:exchange-points-page,
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+<style lang="less" scoped>
+    @import "../../../../css/common.less";
+    #exchange-points-page{
+
+    }
+</style>
